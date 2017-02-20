8d96139d2991f96d56b1d9d5be5582a9437319b9
Artur Bylak <Artur.Bylak@gmail.com>
2017-02-20 22:02:26 +0100

    Init commit

---
+++ src/main/java/com/bylak/mule/esb/spring/Application.java
@@ -0,0 +1,13 @@
+package com.bylak.mule.esb.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
